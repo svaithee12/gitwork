0db405cdfd028d7ab751465512f66188e8eb1dd5
HARINDER SINGH <im.harinder.singh@gmail.com>
2015-10-31 23:35:52 +0530

    Update calc.cpp

--- calc.cpp
+++ calc.cpp
@@ -2,11 +2,11 @@
 int main()
 {
   int a, b, c, d, e, f, j ;
-  c= a+b;
-  d= a-b;
-  e= a%b;
-  f =a/b;
-  j = a*b;
+  c = a + b;
+  d = a - b;
+  e = a % b;
+  f =a / b;
+  j = a * b;
   /// added in the left 
   return 0;
 
